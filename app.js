4854863e051af1dade8700e2f428e47fb44820f0
Aakash Prajapati <aakash.prajapati@trangile.net>
2023-09-10 23:15:22 +0800

    Connection to DB Test

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-const express = require('express')
-const app = express()
-const PORT = 4000
+const express = require('express');
+const oracledb =  require('oracledb');
+const app = express();
+const PORT = 5000;
 
 app.get('/', (req, res) =>{
     res.send("Hello User...")
-})
+});
+
+app.get('/emp', (req, res) =>{
+    async function fetchData(){
+        try {
+            const connection = await oracledb.getConnection({
+                user : 'system',
+                password : 'system',
+                connectionString : 'localhost/xe'
+            });
+
+            const result = await connection.execute('SELECT * FROM EMP WHERE E_ID = 100;');
+            return result.rows;
+
+        } catch (error) {
+            return error;
+        }
+    }
+
+    fetchData()
+    .then(dbRes =>{
+        res.send(dbRes);
+    })
+    .catch(err =>{
+        res.send(err);
+    });
+});
 
 app.listen(PORT,()=>{
     console.log("The server is up and running on port ", PORT)
